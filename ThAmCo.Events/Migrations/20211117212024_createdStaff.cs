a608022e0180ca573cd34e0d10fa6adfa3693bd4
yash61016 <yashpatel61016@gmail.com>
2021-11-17 21:34:00 +0000

    Created Staff model and MVC controllers

---
+++ ThAmCo.Events/Migrations/20211117212024_createdStaff.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Migrations
+{
+    public partial class createdStaff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StaffFirstName = table.Column<string>(nullable: false),
+                    StaffLastName = table.Column<string>(nullable: true),
+                    StaffType = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.StaffId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Staff",
+                columns: new[] { "StaffId", "StaffFirstName", "StaffLastName", "StaffType" },
+                values: new object[,]
+                {
+                    { 1, "Chloye", "Parkinson", "Waiter" },
+                    { 2, "Idris", "Ciftci", "Bar tender" },
+                    { 3, "Stanley", null, "DJ" },
+                    { 4, "Bradley", null, "Photographer" },
+                    { 5, "Candice", "Neistat", "Wedding planner" },
+                    { 6, "Dhruv", "Rathi", "Waiter" },
+                    { 7, "Casey", "Neistat", "Host" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Staff");
+        }
+    }
+}
---
+++ ThAmCo.Events/Models/Staff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Events.Models
+{
+    public class Staff
+    {
+        public Staff()
+        {
+
+        }
+        public Staff(string staffFirstName, string staffLastName, string staffType) : this()
+        {
+            StaffFirstName = staffFirstName;
+            StaffLastName = staffLastName;
+            StaffType = staffType;
+        }
+        [Display(Name = "Staff Id")]
+        public int StaffId { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string StaffFirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        public string StaffLastName { get; set; }
+        [Required]
+        [Display(Name = "Title")]
+        public string StaffType { get; set; }
+    }
+}
