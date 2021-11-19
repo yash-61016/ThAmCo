a38de316aa159137e356e5caa58a33a2ebadce47
yash61016 <yashpatel61016@gmail.com>
2021-11-19 22:21:42 +0000

    Created ViewModel to display list of events

---
+++ ThAmCo.Events/Models/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Events.Models
+{
+    public class Event
+    {
+        public Event()
+        {
+
+        }
+        public int EventId { get; set; }
+        public DateTime EventDateTime { get; set; }
+
+        [Required]
+        public string EventTitle { get; set; }
+
+        [MinLength(3), MaxLength(3)]
+        public string EventTypeId { get; set; }
+
+    }
+}
---
+++ ThAmCo.Events/ViewModels/EventTitleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ThAmCo.Events.DTOs;
+using ThAmCo.Events.Models;
+
+namespace ThAmCo.Events.ViewModels
+{
+    public class EventTitleViewModel
+    {
+
+        public int EventId { get; set; }
+        public DateTime EventDateTime { get; set; }
+
+        [Required]
+        public string EventTitle { get; set; }
+
+       
+
+        [Required]
+        public string EventTypeTitle { get; set; }
+
+    }
+}
